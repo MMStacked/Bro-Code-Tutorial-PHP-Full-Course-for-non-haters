978fb1ebba174d6b5e1afe9eec86f1238087480f
MMStacked <102634576+MMStacked@users.noreply.github.com>
2025-07-09 00:33:25 +0100

    Add files via upload

---
+++ PHP Full Course for non-haters/Completed/Lesson-24-PHP-Connect-to-MySQL-database/index.php
@@ -0,0 +1,39 @@
+<?php
+    // 1. MySQLi Extension
+    // 2. PDO (PHP DATA Object)
+    //    Requires Object Oriented Languages
+
+    // Database ssupported by PDO
+    // 1. MySQL
+    // 2. PostgresSQL
+    // 3. Oracle
+    // 4. Firebird
+    // 5. MS SQL Server
+    // 6. Sybase
+    // 7. Informix
+    // 8. IBM
+    // 9. FreeTDS
+    // 10. SQLite
+    // 11. Cubird
+    // 12. 4d
+
+
+    include("database.php");
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello
+</body>
+</html>
